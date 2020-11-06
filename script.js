892fe532909cca9c91ddc815f9e890a75194ee24
Floy <dmitrievd1995@mail.ru>
2020-11-06 09:30:51 +0000

    Beta3.0

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+'use strict';
+
+let burger = document.getElementById("burger");
+let header = document.getElementById("header");
+let burger__menu = document.getElementById("burger__menu");
+burger.addEventListener("click", Burger);
+
+burger.classList.toggle("burger__open");
+let burger_height = burger__menu.clientHeight;
+burger.classList.toggle("burger__open");
+
+function Burger() {
+  if (burger_height < header.clientHeight)
+    burger__menu.style.height = header.clientHeight + "px";
+  burger.classList.toggle("burger__open");
+}
+
 const anchors = document.querySelectorAll('a[href*="#"]')
 
 for (let anchor of anchors) {
