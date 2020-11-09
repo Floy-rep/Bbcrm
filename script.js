b0c8fa3736e59865541c70a8bb063c0cc25d1a28
Floy <dmitrievd1995@mail.ru>
2020-11-09 13:18:38 +0000

    SignInForm

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 'use strict';
 
+let isopen = false;
+
+let fixed = document.getElementById('fixed');
+let fixed_wrapper = document.getElementById("fixed-wrapper");
 let burger = document.getElementById("burger");
 let header = document.getElementById("header");
 let burger__menu = document.getElementById("burger__menu");
+let header_img = document.getElementById("header_img");
+
 burger.addEventListener("click", Burger);
 
 burger.classList.toggle("burger__open");
@@ -13,10 +19,39 @@ function Burger() {
   if (burger_height < header.clientHeight)
     burger__menu.style.height = header.clientHeight + "px";
   burger.classList.toggle("burger__open");
+  // header_img.style.display = 'none';
+  if (isopen == false)
+  {
+    isopen = true;
+    header_img.style.display = 'none';
+    console.log('1');
+  }
+  else
+  {
+    isopen = false;
+    header_img.style.display = 'inline';
+    console.log('2');
+  }
+
 }
 
-const anchors = document.querySelectorAll('a[href*="#"]')
 
+window.addEventListener('scroll', function() {
+  if (pageYOffset != 0)
+  {
+    fixed.classList.add("header__fixed-active");
+    fixed_wrapper.classList.add("fixed-wrapper");
+  }
+  else
+  {
+    fixed.classList.remove("header__fixed-active");
+    fixed_wrapper.classList.remove("fixed-wrapper");
+  } 
+});
+
+
+
+const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault()
