179b21ad5503dec7cc7a6927816eb3fd4304d4cb
Floy <dmitrievd1995@mail.ru>
2020-11-09 14:17:30 +0000

    SignUp_Alpha

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-let isopen = false;
+// VAR
 
+let isopen = false;
 let fixed = document.getElementById('fixed');
 let fixed_wrapper = document.getElementById("fixed-wrapper");
 let burger = document.getElementById("burger");
@@ -9,32 +10,30 @@ let header = document.getElementById("header");
 let burger__menu = document.getElementById("burger__menu");
 let header_img = document.getElementById("header_img");
 
-burger.addEventListener("click", Burger);
-
 burger.classList.toggle("burger__open");
 let burger_height = burger__menu.clientHeight;
 burger.classList.toggle("burger__open");
 
+// BURGER
+
+burger.addEventListener("click", Burger);
 function Burger() {
   if (burger_height < header.clientHeight)
     burger__menu.style.height = header.clientHeight + "px";
   burger.classList.toggle("burger__open");
-  // header_img.style.display = 'none';
   if (isopen == false)
   {
     isopen = true;
     header_img.style.display = 'none';
-    console.log('1');
   }
   else
   {
     isopen = false;
     header_img.style.display = 'inline';
-    console.log('2');
   }
-
 }
 
+// FIXED MENU
 
 window.addEventListener('scroll', function() {
   if (pageYOffset != 0)
@@ -49,7 +48,7 @@ window.addEventListener('scroll', function() {
   } 
 });
 
-
+// SMOOTH SCROLL
 
 const anchors = document.querySelectorAll('a[href*="#"]')
 for (let anchor of anchors) {
